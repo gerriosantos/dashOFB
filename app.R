de676d27ead38c927cd29b76c09de202c3292b2c
gerriosantos <gerriosantos@gmail.com>
2021-09-15 13:07:36 -0300

    Initial commit

---
+++ app.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(bs4Dash)
+
+
+ui <- dashboardPage(
+  title = "Dash OFB",
+  header = dashboardHeader(),
+  sidebar = dashboardSidebar(),
+  controlbar = dashboardControlbar(),
+  footer = dashboardFooter(),
+  body = dashboardBody()
+)
+
+
+server <- function(input, output, session){}
+
+
+shinyApp(ui, server)
+
+
